7ce40693e6aa04378783b4e8a996bc8b1c848f17
Adam <a.f.pettigrew@lancaster.ac.uk>
2022-06-28 09:10:11 +0100

    added Commands

---
+++ SchnappiBot/src/uk/org/pettigrew/SchnappiBot/Commands.java
@@ -0,0 +1,12 @@
+package uk.org.pettigrew.SchnappiBot;
+
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class Commands extends ListenerAdapter{
+	public void onGuildMessageReceived(GuildMessageReceivedEvent event)
+	{
+		
+	}
+
+}
