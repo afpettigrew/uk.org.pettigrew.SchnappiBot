f26755535058a3cb890011c2ab587fb16c106042
Adam <a.f.pettigrew@lancaster.ac.uk>
2022-06-28 09:09:59 +0100

    filled in SchnappiBot.java

--- SchnappiBot/src/uk/org/pettigrew/SchnappiBot/SchnappiBot.java
+++ SchnappiBot/src/uk/org/pettigrew/SchnappiBot/SchnappiBot.java
@@ -1,5 +1,22 @@
 package uk.org.pettigrew.SchnappiBot;
 
-public class SchnappiBot {
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+
+public class SchnappiBot
+{
+	public static void main(String[] args) throws LoginException
+	{
+		JDABuilder jda = JDABuilder.createDefault("OTkxMjQxMDMzODQ3NDIzMDI4.Gcb5NT.zxGqqKuKh7q_O-Q7ekhHEZI4CPwt4ExonFyMHw");
+		jda.setActivity(Activity.playing("Schnappi schnappi"));
+		jda.setStatus(OnlineStatus.IDLE);
+		jda.addEventListeners(new Commands());
+		jda.build();
+		
+		
+	}
 
 }
